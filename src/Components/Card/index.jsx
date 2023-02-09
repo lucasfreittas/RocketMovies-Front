376121629d9ce12992475ed4ebe5970a80c505c0
Lucas <l.freittas@outlook.com>
2023-02-09 12:36:30 -0300

    Function that determines the number of stars

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,10 +1,16 @@
 import { Container, Header, Main, Footer } from "./styles";
 import { FiStar } from 'react-icons/fi'
 import { Tags } from "../Tags";
-import { useState, useEffect } from "react";
-import { api } from "../../services/axios";
+
 
 export function Card({data, setTaguinha, onClick, ...rest}){
+
+    function handleStars(data){
+       const numberOfStars = Math.ceil(data / 2);
+       return [...Array(numberOfStars)].map((_, index) => (
+        <FiStar key={index}/>
+       ))
+    }
 
     function handleClickedTag(tag){
         setTaguinha(tag.name)
@@ -15,12 +21,7 @@ export function Card({data, setTaguinha, onClick, ...rest}){
             <Header>
                 <h3>{data.title}</h3>
                 <div>
-                    <FiStar/>
-                    <FiStar/>
-                    <FiStar/>
-                    <FiStar/>
-                    <FiStar/>
-
+                    {handleStars(data.rating)}
                 </div>
     
             </Header>
